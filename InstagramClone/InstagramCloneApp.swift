8cde4e182909e2770eb8385496f093ba551fcb32
Deepak2909 <deepak19csu075@ncuindia.edu>
2021-06-08 16:49:27 +0530

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Deepak on 08/06/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
